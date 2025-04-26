bb408f36a920945938dd96c475d9bbf5c0c8f428
VincentCYR7 <yuren1027@gmail.com>
2025-04-26 18:43:33 +0900

    Grid Valid Move Panel Display
    Implemented a system where Panels are shown on the tiles that are valid moves.

--- Assets/Scripts/GridButton.cs
+++ Assets/Scripts/GridButton.cs
@@ -1,8 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridButton : MonoBehaviour
 {
     public Vector2Int gridPosition;
+    //public GameObject panelPrefab;
+    //public Transform parentTransform;
+    private Image panelImage;
+
+    private void Start()
+    {
+        //GameObject panel = Instantiate(panelPrefab, parentTransform);
+        //panel.name = "Panel";
+        panelImage = transform.Find("Panel").GetComponent<Image>();
+        SetPanelTransparent();
+    }
+
+    private void Update()
+    {
+        CheckStatus();
+    }
+
+    public void CheckStatus()
+    {
+        if (InProx(FindObjectOfType<Ghost>().gridPosition))
+        {
+            SetPanelVisible();
+        }
+        else
+        {
+            SetPanelTransparent();
+        }
+    }
+
 
     public void OnClick()
     {
@@ -10,5 +40,27 @@ public class GridButton : MonoBehaviour
         worldPos.z = 0f;
 
         FindObjectOfType<Ghost>().MoveTo(worldPos, gridPosition);
+        SetPanelTransparent();
     }
+
+
+    private void SetPanelVisible()
+    {
+        Color color = panelImage.color;
+        color.a =  0.25f;
+        panelImage.color = color;
+    }
+
+    private void SetPanelTransparent()
+    {
+        Color color = panelImage.color;
+        color.a = 0f;
+        panelImage.color = color;
+    }
+
+    private bool InProx(Vector2Int g)
+    {
+        return Vector2Int.Distance(g, gridPosition) <= 1f;
+    }
+
 }
